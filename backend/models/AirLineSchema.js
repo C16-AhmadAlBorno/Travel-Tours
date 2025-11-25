f6a608e47f2610262c6a1840d276a4d5d5b8be90
AhmadAlBorno <sameerborno.ahmad@gmail.com>
2025-11-26 00:45:23 +0300

    complete 2 schema airline hotels

--- backend/models/AirLineSchema.js
+++ backend/models/AirLineSchema.js
@@ -1,1 +1,34 @@
+const mongoose = require("mongoose");
 
+const airlineSchema = new mongoose.Schema({
+  airline: {
+    type: String,
+    required: true,
+  },
+
+  destination: {
+    type: String,
+    required: true, // KEY destination
+  },
+
+  departureTime: {
+    type: Date,
+    required: true,
+  },
+
+  arrivalTime: {
+    type: Date,
+    required: true,
+  },
+
+  duration: {
+    type: Number, // minutes or hours
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+module.exports = mongoose.model("Airline", airlineSchema);
