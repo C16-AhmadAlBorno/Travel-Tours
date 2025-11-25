4f6c47abd1721c01646ad779c7176cbda1138a54
AhmadAlBorno <sameerborno.ahmad@gmail.com>
2025-11-26 01:11:50 +0300

    complete routers

---
+++ backend/Routes/customers.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const customerRouter = express.Router();
+
+module.exports = customerRouter;
---
+++ backend/Routes/hotels.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const hotelRouter = express.Router();
+
+module.exports = hotelRouter;
