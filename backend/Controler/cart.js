310c1dae43d22ec591fbff69a5c97ac956f01eb0
AhmadAlBorno <sameerborno.ahmad@gmail.com>
2025-12-05 00:04:17 +0300

    cart update done

--- backend/Controler/cart.js
+++ backend/Controler/cart.js
@@ -41,24 +41,52 @@ const allCartItem = (req, res) => {
 //=================delete from cart============
 const cartDeleted = (req, res) => {
   const { _id } = req.params;
-  console.log(_id);
 
   cartModel
     .findByIdAndDelete({ _id })
     .then((result) => {
       res.status(200).json({
         success: true,
-        message:"item deleted succsfuly",
+        message: "item deleted succsfuly",
         result,
       });
     })
     .catch((err) => {
       res.status(500).json({
         success: false,
-                message:"error server",
+        message: "error server",
 
         err,
       });
     });
 };
-module.exports = { addToCart, allCartItem, cartDeleted };
+//================== update item in cart ==================
+const updateCartItem = (req, res) => {
+  const { id } = req.params;
+  const { tourpackage } = req.body;
+
+  cartModel
+    .findByIdAndUpdate(id, { tourpackage }, { new: true })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: "Item not found",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Item updated successfully",
+        result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err,
+      });
+    });
+};
+module.exports = { addToCart, allCartItem, cartDeleted ,updateCartItem};
--- backend/Routes/cart.js
+++ backend/Routes/cart.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { addToCart, allCartItem, cartDeleted } = require("../Controler/cart");
+const {
+  addToCart,
+  allCartItem,
+  cartDeleted,
+  updateCartItem,
+} = require("../Controler/cart");
 const cartRouter = express.Router();
 cartRouter.post("/new", addToCart);
 cartRouter.get("/items", allCartItem);
 cartRouter.delete("/delete/:_id", cartDeleted);
+cartRouter.put("/update/:_id", updateCartItem);
 module.exports = cartRouter;
